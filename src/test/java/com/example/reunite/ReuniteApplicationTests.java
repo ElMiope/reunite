d0fc4dc9c4588b613088a8bf09ff27a80a81f402
ElMiope <maxiceisa77@gmail.com>
2026-07-21 12:25:33 -0300

    Cree el proyecto

---
+++ src/test/java/com/example/reunite/ReuniteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reunite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReuniteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
